eebf8d05df6fefe08d6698f089febf2ea4fdd0eb
Yura Istomin <yura.ist@icloud.com>
2022-01-12 11:12:44 +0300

    Add tank actions

--- StrangeBody/Tank/ARTankViewContainer.swift
+++ StrangeBody/Tank/ARTankViewContainer.swift
@@ -2,15 +2,23 @@ import ARKit
 import SwiftUI
 import RealityKit
 
-struct ARTankViewContainer: UIViewRepresentable {
-  
-  func makeUIView(context: Context) -> ARView {
-    let arView = ARView()
-    let tankAnchor = try! TinyToyTank.load_TinyToyTank()
-    arView.scene.anchors.append(tankAnchor)
-    
-    return arView
+//struct ARTankViewContainer: UIViewRepresentable {
+//
+//  func makeUIView(context: Context) -> ARView {
+//    let arView = ARView()
+//    let tankAnchor = try! TinyToyTank.load_TinyToyTank()
+//    arView.scene.anchors.append(tankAnchor)
+//
+//    return arView
+//  }
+//
+//  func updateUIView(_ uiView: ARView, context: Context) {}
+//}
+
+struct ARTankViewContainer: UIViewControllerRepresentable {
+  func makeUIViewController(context: Context) -> ARViewController {
+    return ARViewController()
   }
   
-  func updateUIView(_ uiView: ARView, context: Context) {}
+  func updateUIViewController(_ uiViewController: ARViewController, context: Context) {}
 }
--- StrangeBody/Tank/ARViewController.swift
+++ StrangeBody/Tank/ARViewController.swift
@@ -8,11 +8,19 @@ class ARViewController: UIViewController {
    
   private var tankAnchor: TinyToyTank._TinyToyTank?
   
+  private let tankRightButton = UIButton()
+  private let tankLeftButton = UIButton()
+  private let tankForwardButton = UIButton()
+  private let turretRightButton = UIButton()
+  private let turretLeftButton = UIButton()
+  private let fireButton = UIButton()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   
     addARView()
     addTank()
+    addButtons()
   }
   
   private func addARView() {
@@ -27,8 +35,56 @@ class ARViewController: UIViewController {
     ])
   }
   
-  func addTank() {
+  private func addTank() {
     tankAnchor = try! TinyToyTank.load_TinyToyTank()
     arView.scene.anchors.append(tankAnchor!)
+  }
+  
+  private func addButtons() {
+    let firstStackView = UIStackView(arrangedSubviews: [tankLeftButton, tankForwardButton, tankRightButton])
+    firstStackView.axis = .horizontal
+    
+    let secondStackView = UIStackView(arrangedSubviews: [turretLeftButton, fireButton, turretRightButton])
+    secondStackView.axis = .horizontal
+    
+    tankRightButton.setImage(UIImage(named: "TankRight"), for: .normal)
+    tankLeftButton.setImage(UIImage(named: "TankLeft"), for: .normal)
+    tankForwardButton.setImage(UIImage(named: "TankForward"), for: .normal)
+    turretRightButton.setImage(UIImage(named: "TurretRight"), for: .normal)
+    turretLeftButton.setImage(UIImage(named: "TurretLeft"), for: .normal)
+    fireButton.setImage(UIImage(named: "CannonFire"), for: .normal)
+    
+    let thirdStackView = UIStackView(arrangedSubviews: [firstStackView, secondStackView])
+    thirdStackView.axis = .vertical
+    
+    view.addSubview(thirdStackView)
+    thirdStackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    thirdStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    thirdStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80).isActive = true
+    
+    tankRightButton.addAction(UIAction(handler: { _ in
+      self.tankAnchor?.notifications.tankRight.post()
+    }), for: .touchUpInside)
+    
+    tankLeftButton.addAction(UIAction(handler: { _ in
+      self.tankAnchor?.notifications.tankLeft.post()
+    }), for: .touchUpInside)
+    
+    tankForwardButton.addAction(UIAction(handler: { _ in
+      self.tankAnchor?.notifications.tankForward.post()
+    }), for: .touchUpInside)
+    
+    turretRightButton.addAction(UIAction(handler: { _ in
+      self.tankAnchor?.notifications.turretRight.post()
+    }), for: .touchUpInside)
+    
+    turretLeftButton.addAction(UIAction(handler: { _ in
+      self.tankAnchor?.notifications.turretLeft.post()
+    }), for: .touchUpInside)
+    
+    fireButton.addAction(UIAction(handler: { _ in
+      self.tankAnchor?.notifications.cannonFire.post()
+    }), for: .touchUpInside)
   }
 }
